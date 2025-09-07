e0cc0163c99711e82ccaf711385e841c206f8650
mohammad hajjaj <mohd.hajjaj80@gmail.com>
2025-09-08 01:19:03 +0300

    setting db

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,26 +1,12 @@
 import express from "express";
 
 import notesRoutes from "./routes/notesRoutes.js";
-
+import connectDB from "./config/db.js";
 const app = express();
-
+const PORT = process.env.PORT || 3000;
 app.use("/api/notes", notesRoutes);
+connectDB();
 
-// app.get('/', (req, res) => {
-//     res.send('Hello World122!');
-// });
-
-// app.post('/data', (req, res) => {
-// res.status(201).send('Data received');
-// });
-// app.put('/data/:id', (req, res) => {
-//     res.status(200).send('Data updated');
-// });
-
-// app.delete('/data/:id', (req, res) => {
-//     res.status(200).send('Data deleted');
-// });
-
-app.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
