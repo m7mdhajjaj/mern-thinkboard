5be43a115846cb367d9ddd926bdf761841e5cd43
mohammad hajjaj <mohd.hajjaj80@gmail.com>
2025-09-09 01:36:26 +0300

    hot toast

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -3,9 +3,11 @@ import {Route, Routes} from "react-router-dom";
 import Home from "./pages/HomePage";
 import NotePage from "./pages/Note";
 import Create from "./pages/CreatePAge";
+import toast from  'react-hot-toast';
 const App = () => {
   return (
     <div>
+      <button onClick={()=>toast.success('hello')}>click me</button>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/note/:id' element={<NotePage/>}/>
